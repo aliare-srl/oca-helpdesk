3d96e8a9f6912d9838a5995bf49933d671235e26
Aliaresrl <contactos@aliare.com.ar>
2026-04-24 12:59:40 +0000

    update: oca-helpdesh

--- helpdesk_mgmt/models/helpdesk_ticket.py
+++ helpdesk_mgmt/models/helpdesk_ticket.py
@@ -18,7 +18,7 @@ class HelpdeskTicket(models.Model):
     @api.depends("stage_id.name")
     def _compute_stage_color_index(self):
         color_map = {
-            "Nuevo": 7,       "New": 7,
+            "Nuevo": 8,       "New": 8,
             "En progreso": 4, "In Progress": 4,
             "En espera": 3,   "Awaiting": 3,
             "Hecho": 10,      "Done": 10,
