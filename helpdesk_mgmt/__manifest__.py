6f5117fba6240c3a764a3aaf1e522adb80cacbfd
Aliaresrl <contactos@aliare.com.ar>
2026-05-05 20:26:56 -0300

    update: oca-helpdesk

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -42,6 +42,7 @@
             "helpdesk_mgmt/static/src/js/helpdesk_tab_fix.js",
             "helpdesk_mgmt/static/src/js/helpdesk_list_tab_add.js",
             "helpdesk_mgmt/static/src/js/helpdesk_list_save_reload.js",
+            "helpdesk_mgmt/static/src/js/helpdesk_sla_notifications.js",
         ],
         "web.assets_frontend": [
             "helpdesk_mgmt/static/src/js/new_ticket.js",
