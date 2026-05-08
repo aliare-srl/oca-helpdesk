f0df45dc5d6e4f75c117aa71f25ef641774c4667
Aliaresrl <143626551+Aliaresrl@users.noreply.github.com>
2026-05-08 12:48:41 -0300

    Add files via upload

--- helpdesk_mgmt_rating/models/helpdesk_ticket.py
+++ helpdesk_mgmt_rating/models/helpdesk_ticket.py
@@ -40,29 +40,14 @@ class HelpdeskTicket(models.Model):
                 self._send_ticket_rating_mail(force_send=False)
         return res
 
-    def _get_parent_cc_email(self):
-        self.ensure_one()
-        if self.partner_id and self.partner_id.parent_id:
-            parent = self.partner_id.commercial_partner_id
-            if parent and parent.email:
-                return parent.email_formatted
-        return None
-
     def _send_ticket_rating_mail(self, force_send=False):
         for ticket in self:
             if ticket.rating_status == "stage_change":
                 survey_template = ticket.stage_id.rating_mail_template_id
                 if survey_template:
-                    cc_email = ticket._get_parent_cc_email()
-                    email_values = {}
-                    if cc_email:
-                        email_values["email_cc"] = cc_email
-                    lang = ticket.partner_id.lang
-                    tmpl = survey_template.with_context(lang=lang) if lang else survey_template
-                    tmpl.send_mail(
-                        ticket.id,
-                        email_values=email_values,
-                        notif_layout=False,
+                    ticket.rating_send_request(
+                        survey_template,
+                        lang=ticket.partner_id.lang,
                         force_send=force_send,
                     )
 
